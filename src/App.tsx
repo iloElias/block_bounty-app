b8a0c7e49416a3ecbd5f6b956a2781ae16eef45d
murilo.elias <muriloelias@ipeweb.com.br>
2024-04-03 02:25:50 -0300

    More blocks update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import './App.css'
+import './css/App.css'
 import './css/Variables.css'
 import './css/Animations.css'
 import './css/Cube.css'
-import Cube from './components/Cube';
+import './css/PageEvents.css';
+import { Cobblestone, CommandBlock, CraftingTable, Dirt, GrassBlock, OakPlank } from './components/Cube';
 
 
 function App() {
   return (
     <div className='app'>
-      <Cube />
+      {/* <OakPlank />
+      <CraftingTable />
+      <GrassBlock />
+      <Cobblestone />
+      <Dirt /> */}
+      <CommandBlock />
     </div>
   )
 }
--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,115 +1,300 @@
-// import { minecraftSimplified } from "../templates/Cubes";
-
 import dirt from "../assets/images/minecraft-simplified/dirt.png";
 import grassSides from "../assets/images/minecraft-simplified/grass_block_side_overlay.png";
 import grassTop from "../assets/images/minecraft-simplified/grass_block_top.png";
+import craftingTableTop from "../assets/images/minecraft-simplified/crafting_table_top.png";
+import craftingTableFront from "../assets/images/minecraft-simplified/crafting_table_front.png";
+import craftingTableSide from "../assets/images/minecraft-simplified/crafting_table_side.png";
+import oakPlank from "../assets/images/minecraft-simplified/oak_planks.png";
+import cobblestone from "../assets/images/minecraft-simplified/cobblestone.png";
+import commandBlockFront from "../assets/images/minecraft-simplified/command_block_front.png";
+import commandBlockBack from "../assets/images/minecraft-simplified/command_block_back.png";
+import commandBlockSide from "../assets/images/minecraft-simplified/command_block_side.png";
+import furnaceFront from "../assets/images/minecraft-simplified/furnace_front.png";
+import furnaceSide from "../assets/images/minecraft-simplified/furnace_side.png";
+import furnaceTop from "../assets/images/minecraft-simplified/furnace_top.png";
+import glass from "../assets/images/minecraft-simplified/glass.png";
+import glowstone from "../assets/images/minecraft-simplified/glowstone.png";
+import copperBlock from "../assets/images/minecraft-simplified/copper_block.png";
+import ironBlock from "../assets/images/minecraft-simplified/iron_block.png";
+import goldBlock from "../assets/images/minecraft-simplified/gold_block.png";
+import diamondBlock from "../assets/images/minecraft-simplified/diamond_block.png";
+import netheriteBlock from "../assets/images/minecraft-simplified/netherite_block.png";
+import sand from "../assets/images/minecraft-simplified/sand.png";
+import noteBlock from "../assets/images/minecraft-simplified/note_block.png";
+import observerFront from "../assets/images/minecraft-simplified/observer_front.png";
+import observerBack from "../assets/images/minecraft-simplified/observer_back.png";
+import observerSide from "../assets/images/minecraft-simplified/observer_side.png";
+import observerTop from "../assets/images/minecraft-simplified/observer_top.png";
+import observerBottom from "../assets/images/minecraft-simplified/stonecutter_bottom.png";
+
+function instanceOfFaces(object: any): object is Faces {
+  if (!object) {
+    return false;
+  }
+
+  return (
+    "top" in object
+    || "front" in object
+    || "left" in object
+    || "back" in object
+    || "right" in object
+    || "bottom" in object
+  );
+}
+
+interface FaceLayers {
+  above?: string;
+  middle?: string;
+  under?: string;
+}
+
+interface Faces {
+  top?: FaceLayers | string;
+  front?: FaceLayers | string;
+  left?: FaceLayers | string;
+  back?: FaceLayers | string;
+  right?: FaceLayers | string;
+  bottom?: FaceLayers | string;
+}
 
 interface CubeProps {
   cubeAnimation?: string;
-  texture?: string;
-  topAbove?: string;
-  topUnder?: string;
-  sidesAbove?: string;
-  sidesUnder?: string;
-  bottomAbove?: string;
-  bottomUnder?: string;
+  texture?: Faces | string;
+  className?: string;
+  colorTint?: string;
+  customFilter?: string;
 }
 
-export default function Cube({
+function Cube({
+  // cubeAnimation,
   texture,
-  topAbove,
-  topUnder,
-  sidesAbove,
-  sidesUnder,
-  bottomAbove,
-  bottomUnder
+  className,
+  colorTint,
+  customFilter,
 }: CubeProps) {
-  const textureHolder = texture ?? dirt;
-
-  return (
-    <div className="scene">
-      <div className="cube">
-        <div className="cube-face cube-face-top">
-          <div className="image-above-overlay" />
-          {/* <img className="face-default image-above" src={topAbove ?? grassTop} /> */}
-          <div className="image-under-overlay" />
-          <img className="face-default image-under" src={topUnder ?? textureHolder} />
-        </div>
-        <div className="cube-face cube-face-front">
-          <div className="image-above-overlay" />
-          {/* <img className="face-default image-above" src={sidesAbove ?? grassSides} /> */}
-          <div className="image-under-overlay" />
-          <img className="face-default image-under" src={sidesUnder ?? textureHolder} />
-        </div>
-        <div className="cube-face cube-face-back">
-          <div className="image-above-overlay" />
-          {/* <img className="face-default image-above" src={sidesAbove ?? grassSides} /> */}
-          <div className="image-under-overlay" />
-          <img className="face-default image-under" src={sidesUnder ?? textureHolder} />
-        </div>
-        <div className="cube-face cube-face-right">
-          <div className="image-above-overlay" />
-          {/* <img className="face-default image-above" src={sidesAbove ?? grassSides} /> */}
-          <div className="image-under-overlay" />
-          <img className="face-default image-under" src={sidesUnder ?? textureHolder} />
-        </div>
-        <div className="cube-face cube-face-left">
-          <div className="image-above-overlay" />
-          {/* <img className="face-default image-above" src={sidesAbove ?? grassSides} /> */}
-          <div className="image-under-overlay" />
-          <img className="face-default image-under" src={sidesUnder ?? textureHolder} />
-        </div>
-        <div className="cube-face cube-face-bottom">
-          <div className="image-above-overlay" />
-          <img className="face-default image-above" src={bottomAbove} />
-          <div className="image-under-overlay" />
-          <img className="face-default image-under" style={{ rotate: "270deg" }} src={bottomUnder ?? textureHolder} />
+  if (typeof texture === "string") {
+    return (
+      <div className="scene">
+        <div className={`cube${className ? " " + className : ""}`}>
+          <div className="cube-face cube-face-top">
+            <Face texture={texture} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-front">
+            <Face texture={texture} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-left">
+            <Face texture={texture} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-back">
+            <Face texture={texture} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-right">
+            <Face texture={texture} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-bottom">
+            <Face texture={texture} color={colorTint} customFilter={customFilter} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else if (instanceOfFaces(texture)) {
+    const {
+      top,
+      front,
+      left,
+      back,
+      right,
+      bottom
+    } = texture;
+
+    return (
+      <div className="scene">
+        <div className={`cube${className ? " " + className : ""}`}>
+          <div className="cube-face cube-face-top">
+            <Face texture={top} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-front">
+            <Face texture={front} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-left">
+            <Face texture={left} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-back">
+            <Face texture={back} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-right">
+            <Face texture={right} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-bottom">
+            <Face texture={bottom} color={colorTint} customFilter={customFilter} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="scene">
+        <div className={`cube${className ? " " + className : ""}`}>
+          <div className="cube-face cube-face-top">
+            <Face texture={dirt} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-front">
+            <Face texture={dirt} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-left">
+            <Face texture={dirt} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-back">
+            <Face texture={dirt} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-right">
+            <Face texture={dirt} color={colorTint} customFilter={customFilter} />
+          </div>
+          <div className="cube-face cube-face-bottom">
+            <Face texture={dirt} color={colorTint} customFilter={customFilter} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 interface FaceProps {
-  face?: string,
-  faceOverlay?: string,
-  middle?: string,
-  middleOverlay?: string,
-  under?: string,
-  underOverlay?: string,
-  overlay?: string,
-  overlayColor?: string
+  texture?: FaceLayers | string;
+  color?: string;
+  customFilter?: string;
 }
 
 export function Face({
-  face,
-  faceOverlay,
-  middle,
-  middleOverlay,
-  under,
-  underOverlay,
-  overlay,
-  overlayColor
+  texture,
+  color,
+  customFilter,
 }: FaceProps) {
+  const colorTint = { filter: `drop-shadow(calc(var(--loading-cube-size) * -1) 0 0px ${color}) ${customFilter ? customFilter : ""}` };
 
   return (
     <div className="face-wrapper">
-      <div className="face-layer-above">
-        {faceOverlay && (<img className="face-overlay image-above-overlay" style={overlayColor ? { filter: `drop-shadow(0 0 0 ${overlayColor})` } : {}} src={faceOverlay} />)}
-        <img className="face-default image-above" src={face} />
-      </div>
-      {middle && (
-        <div className="face-layer-above">
-          {middleOverlay && (<img className="face-overlay image-middle-overlay" style={overlayColor ? { filter: `drop-shadow(0 0 0 ${overlayColor})` } : {}} src={middleOverlay} />)}
-          <img className="face-default image-middle" src={middle} />
-        </div>
-      )}
-      {overlay && (
-        <div className="face-layer-above">
-          {underOverlay && (<img className="face-overlay image-under-overlay" style={overlayColor ? { filter: `drop-shadow(0 0 0 ${overlayColor})` } : {}} src={underOverlay} />)}
-          <img className="face-default image-under" src={under} />
-        </div>
-      )}
+      {
+        typeof texture === "string" ? (
+          <div className="face-layer-above">
+            <img className="face-overlay image-above" src={texture} />
+          </div>
+        ) : (
+          <>
+            {texture?.above && (
+              <>
+                <div className="face-layer-above" style={colorTint}>
+                  <img className="face-overlay image-above" src={texture?.above} />
+                </div>
+                {color && (<img className="texture-image image-above" src={texture?.above} />)}
+              </>
+            )}
+            {texture?.middle && (<div className="face-layer-middle">
+              <img className="face-overlay image-middle" src={texture?.middle} />
+            </div>)}
+            {texture?.under && (<div className="face-layer-under">
+              <img className="face-overlay image-under" src={texture?.under} />
+            </div>)}
+          </>
+        )
+      }
     </div>
   );
+}
+
+interface CubeTemplateProps {
+  texture: Faces;
+  colorTint?: string;
+  className?: string;
+  customFilter?: string;
+}
+
+function CubeTemplate(props: CubeTemplateProps) {
+  return (<Cube {...props} />);
+}
+
+export function Dirt() {
+  return CubeTemplate({
+    className: "dirt",
+    texture: {
+      top: { under: dirt },
+      front: { under: dirt },
+      left: { under: dirt },
+      back: { under: dirt },
+      right: { under: dirt },
+      bottom: { under: dirt }
+    }
+  });
+}
+
+export function GrassBlock() {
+  return CubeTemplate({
+    className: "grass-block",
+    texture: {
+      top: { above: grassTop, under: dirt },
+      front: { above: grassSides, under: dirt },
+      left: { above: grassSides, under: dirt },
+      back: { above: grassSides, under: dirt },
+      right: { above: grassSides, under: dirt },
+      bottom: { under: dirt }
+    },
+    colorTint: "#4CBA31"
+  });
+}
+
+export function CraftingTable() {
+  return CubeTemplate({
+    className: "crafting-table",
+    texture: {
+      top: { under: craftingTableTop },
+      front: { under: craftingTableFront },
+      left: { under: craftingTableSide },
+      back: { under: craftingTableFront },
+      right: { under: craftingTableSide },
+      bottom: { under: oakPlank }
+    }
+  });
+}
+
+export function OakPlank() {
+  return CubeTemplate({
+    className: "oak-plank",
+    texture: {
+      top: { under: oakPlank },
+      front: { under: oakPlank },
+      left: { under: oakPlank },
+      back: { under: oakPlank },
+      right: { under: oakPlank },
+      bottom: { under: oakPlank }
+    }
+  });
+}
+
+export function Cobblestone() {
+  return CubeTemplate({
+    className: "cobblestone",
+    texture: {
+      top: { under: cobblestone },
+      front: { under: cobblestone },
+      left: { under: cobblestone },
+      back: { under: cobblestone },
+      right: { under: cobblestone },
+      bottom: { under: cobblestone }
+    }
+  });
+}
+
+export function CommandBlock() {
+  return CubeTemplate({
+    className: "command-block",
+    texture: {
+      top: { under: commandBlockSide },
+      front: { under: commandBlockFront },
+      left: { under: commandBlockSide },
+      back: { under: commandBlockBack },
+      right: { under: commandBlockSide },
+      bottom: { under: commandBlockSide }
+    }
+  });
 }
